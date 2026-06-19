6cc34ff7a3e4f7f84f8505b59ed90b67ba8e2777
SandPotNoodles <77805958+ActuallySandPotNoodles@users.noreply.github.com>
2026-06-19 12:51:03 +0100

    Create install.sh for 'diginum' installation
    The script checks for root privileges, downloads necessary files, and installs the 'diginum' application based on the architecture.

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+$ARCH = $(uname -m)
+echo "Starting Install Of 'diginum'..."
+echo "Downloading Stuff..."
+mkdir -p /usr/share/sandpotnoodles
+wget https://raw.githubusercontent.com/ActuallySandPotNoodles/diginum-cpp/refs/heads/main/diginum.png -O /usr/share/sandpotnoodles/diginum.png
+wget https://raw.githubusercontent.com/ActuallySandPotNoodles/diginum-cpp/refs/heads/main/diginum.desktop -O /usr/share/applications/diginum.desktop
+if [[ "$(uname -m)" == "x86_64" ]]; then
+wget https://github.com/ActuallySandPotNoodles/diginum-cpp/releases/download/v1.0.0/diginum_x86_64 -O /usr/bin/sanddice
+fi
+if [[ "$(uname -m)" == "aarch64" ]]; then
+wget https://github.com/ActuallySandPotNoodles/diginum-cpp/releases/download/v1.0.0/diginum_aarch64 -O /usr/bin/sanddice
+fi
+chmod 775 /usr/bin/sanddice
+echo "Install Complete!"
